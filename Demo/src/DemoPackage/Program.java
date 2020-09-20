0e71b413eeeab01c0056570e9965f54cde14de1d
deepak6git <70337075+deepak6git@users.noreply.github.com>
2020-09-21 00:57:36 +0530

    for demo maven test
    my maven project

---
+++ Demo/src/DemoPackage/Program.java
@@ -0,0 +1,14 @@
+package DemoPackage;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World");
+		System.out.println("Please enter your name");
+		int a= 10;
+		
+		System.out.println(a);
+	}
+
+}
